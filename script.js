71eb674a54469060f3e6afdabe0cf4ebe70ff572
Jenerik <proximity2006@gmail.com>
2021-01-13 17:16:48 +0600

    changed slider, added loadedHandler

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-  $('.banner-slider').slick({
-    slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  });
-});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const images = document.querySelectorAll('.portfolio__image')
+  images.forEach(portfolio__image => {
+    portfolio__image.addEventListener('load', loadedHandler)
+})
+
+function loadedHandler(event) {
+  event.path[0].setAttribute('src', event.path[0].dataset.src)
+  event.path[0].removeEventListener('load', loadedHandler)
+}
+})
