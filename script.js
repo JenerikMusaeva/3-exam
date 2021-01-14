ba0e49b7b60386e4a2e06b37bbb849ba739645e8
Jenerik <proximity2006@gmail.com>
2021-01-15 01:29:11 +0600

    changed all files

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('.portfolio__image')
-  images.forEach(portfolio__image => {
-    portfolio__image.addEventListener('load', loadedHandler)
-})
+// script for 'when the picture is loaded'
+// document.addEventListener('DOMContentLoaded', () => {
+//   const images = document.querySelectorAll('.portfolio__image')
+//   images.forEach(portfolio__image => {
+//     portfolio__image.addEventListener('load', loadedHandler)
+//   })
 
-function loadedHandler(event) {
-  event.path[0].setAttribute('src', event.path[0].dataset.src)
-  event.path[0].removeEventListener('load', loadedHandler)
-}
-})
+//   function loadedHandler(event) {
+//     event.path[0].setAttribute('src', event.path[0].dataset.src)
+//     event.path[0].removeEventListener('load', loadedHandler)
+//   }
+// })
 
 // script for modal image
 
 window.onload = function () {
   const modal = document.querySelector('.modal-img')
-  const portfolioItem = document.querySelectorAll('.portfolio__item')
+  const portfolioItem = document.querySelectorAll('.portfolio__image')
+  const span = document.getElementsByClassName('close-modal-img')[0]
 
   portfolioItem.forEach(image => {
-    image.addEventListener('click', (event) => {
-      modal.style.display = 'inline-flex';
+    image.addEventListener('click', event => {
+      modal.style.display = 'inline-flex'
       const modalImg = modal.querySelector('.modal-window')
-      // const portfolioImage = document.querySelectorAll('.portfolio__image')
-      if (modalImg.children[1]) modalImg.children[1].remove()
+      if (modalImg.children[1]) modalImg.children[1].remove();
       modalImg.append(image.cloneNode())
-      console.log(image.cloneNode())
     })
   })
-  const span = document.getElementsByClassName('close-modal-img')[0]
-  span.onclick = function () {
+
+  span.addEventListener('click', function(e){
+    e.preventDefault();
     modal.style.display = 'none'
-  }
+  })
+
+  modal.addEventListener('click', function(e){
+    e.preventDefault();
+    modal.style.display = 'none'
+  })
 }
